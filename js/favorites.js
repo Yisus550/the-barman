af0b79b66a2fae1d658f1a4aefe7467459faa8be
Yisus550 <jesus.alfonso2402@gmail.com>
2025-05-11 14:18:10 -0700

    feat: Enhance favorites functionality with recipe modal and delete option

--- js/favorites.js
+++ js/favorites.js
@@ -33,5 +33,75 @@ document.addEventListener("DOMContentLoaded", () => {
             </div>
       `;
     drinkList.appendChild(drinkCard);
+
+    document.querySelectorAll(".view-recipe-btn").forEach((btn) => {
+      btn.addEventListener("click", (e) => {
+        const recipeId = e.target.closest("article").id;
+        const recipe = savedRecipes.find((r) => r.id === recipeId);
+
+        if (recipe) {
+          const modalContent = document.getElementById("recipe-modal");
+          modalContent.innerHTML = `
+                <div class="modal-header">
+              <img
+              src="${recipe.image}/medium"
+              alt="${recipe.name}"
+              />
+              <h3>${recipe.name}</h3>
+            </div>
+
+            <div class="modal-body">
+              <div class="modal-ingredients">
+              <h4>Ingredientes</h4>
+              <ul class="ingredients-list">
+                  ${recipe.ingredients}
+              </ul>
+            </div>
+
+              ${
+                recipe.instructions
+                  ? `<div class="modal-instructions">
+                      <h4>Instrucciones</h4>
+                      <p>${recipe.instructions}</p>
+                    </div>`
+                  : ""
+              }
+
+              <button class="btn-primary delete-recipe">Eliminar de favoritos</button>
+            </div>
+            `;
+        }
+
+        const modal = document.querySelector(".modal");
+        modal.classList.remove("hidden");
+        document.body.style.overflow = "hidden";
+
+        document
+          .querySelector(".delete-recipe")
+          .addEventListener("click", () => {
+            const updatedRecipes = savedRecipes.filter(
+              (r) => r.id !== recipeId
+            );
+
+            localStorage.setItem(
+              "savedRecipes",
+              JSON.stringify(updatedRecipes)
+            );
+
+            modal.classList.add("hidden");
+            document.body.style.overflow = "auto";
+            drinkCard.remove();
+
+            if (updatedRecipes.length === 0) {
+              const emptyMessage = document.createElement("p");
+              emptyMessage.textContent = "No tienes recetas guardadas.";
+              favoritesContainer.appendChild(emptyMessage);
+            }
+
+            alert("Receta eliminada de favoritos!");
+            window.location.reload();
+          });
+      });
+    });
   });
 });
